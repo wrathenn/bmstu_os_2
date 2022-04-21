7fe04724ea8384f950d329a5887b049f7a02b644
rostislav <kluskape@gmail.com>
2022-04-21 16:02:40 +0300

    lab_2 extr

--- lab_2/src/main.c
+++ lab_2/src/main.c
@@ -21,8 +21,39 @@ static int walk(char *nextf, int offset)
 
     if (lstat(nextf, &statbuf) == -1)
     {
-        printf("Ошибка вызова lstat\n");
-        return -1;
+        printf("Ошибка вызова lstat: ");
+        switch (errno)
+        {
+            case EACCES:
+                printf("нет доступа к директории\n");
+                break;
+
+            case EIO:
+                printf("ошибка чтения из файловой системы\n");
+                break;
+
+            case ELOOP:
+                printf("произошло зацикливание символьной ссылки\n");
+                break;
+
+            case ENAMETOOLONG:
+                printf("слишком длинное имя\n");
+                break;
+
+            case ENOTDIR:
+                printf("не директория\n");
+                break;
+
+            case ENOENT:
+                printf("директории не существует или передана пустая строка\n");
+                break;
+
+            case EOVERFLOW:
+                printf("переполнение буфера\n");
+                break;
+        }
+
+        return errno;
     }
 
     printfname(offset, nextf, &statbuf);
@@ -37,16 +68,16 @@ static int walk(char *nextf, int offset)
      */
     if (chdir(nextf) != 0)
     {
-    	printf("!!! Нет доступа к директории %s\n", nextf);
+    	printf("Нет доступа к директории %s\n", nextf);
     	return 0;
     }
-    
+
     if ((dp = opendir(".")) == 0)
     {
         printf("Ошибка при открытии директории %s\n", nextf);
         return -1;
     }
-    
+
     while ((dirp = readdir(dp)) != 0 && ret == 0)
         if (strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0)
             ret = walk(dirp->d_name, offset + 2); // Рекурсия
